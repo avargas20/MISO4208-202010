f44ec27a049960c8663427862f098e3cb8de4ede
Nicolas Lema <n.lema@uniandes.edu.co>
2020-02-16 00:54:00 -0500

    Primer implementacion del models.py

--- pruebas_app/models.py
+++ pruebas_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+# Primer mundo
+
+class Aplicacion(models.Model):
+    nombre = models.CharField(max_length=30)
+    descripcion = models.TextField(verbose_name='descripción')
+    TIPOS = (
+    ("movil", "móvil"),
+    ("web", "web"), 
+            )     
+    tipo = models.CharField(max_length=10, choices=TIPOS) 
+
+class Version(models.Model):
+    numero = models.CharField(max_length=30)
+    descripcion = models.TextField(verbose_name='descripción')
+    aplicacion = models.ForeignKey(Aplicacion, on_delete=models.CASCADE)
+
+class Estrategia(models.Model):
+    nombre = models.CharField(max_length=30)
+    descripcion = models.TextField(verbose_name='descripción')
+    version = models.ForeignKey(Version, on_delete=models.CASCADE)
+
+class Herramienta(models.Model):
+    nombre = models.CharField(max_length=30)
+    descripcion = models.TextField(verbose_name='descripción')
+
+class Tipo(models.Model):
+    nombre = models.CharField(max_length=30)
+    descripcion = models.TextField(verbose_name='descripción')
+
+class Prueba(models.Model):
+    script = models.FileField(upload_to='scripts/')
+    tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
+    estrategia = models.ForeignKey(Estrategia, on_delete=models.CASCADE)
+    herramienta = models.ForeignKey(Herramienta, on_delete=models.CASCADE)
