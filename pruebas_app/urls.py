2af110e42ded1022d5f8e117c35b3d539d6df41c
Nicolas Lema <n.lema@uniandes.edu.co>
2020-02-16 23:27:37 -0500

    Implementacion de lanzar una estrategia y crearla

--- pruebas_app/urls.py
+++ pruebas_app/urls.py
@@ -5,4 +5,6 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    path('agregar_estrategia/', views.agregar_estrategia, name='agregar_estrategia'),
+    path('lanzar_estrategia/', views.lanzar_estrategia, name='lanzar_estrategia'),
 ]
--- pruebas_app/views.py
+++ pruebas_app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import Aplicacion, Version, Herramienta, Tipo, Estrategia
 
 # Create your views here.
 
 def home(request):
     return render(request, 'pruebas_app/index.html')
+
+def agregar_estrategia(request):
+    aplicaciones = Aplicacion.objects.all()
+    versiones = Version.objects.all() #Hay que sacar la versiones de la app seleccionada, aqui hay un tuto https://simpleisbetterthancomplex.com/tutorial/2018/01/29/how-to-implement-dependent-or-chained-dropdown-list-with-django.html
+    herramientas = Herramienta.objects.all()
+    tipos = Tipo.objects.all()
+    return render(request, 'pruebas_app/agregar_estrategia.html', {'aplicaciones': aplicaciones, 'versiones': versiones, 'herramientas': herramientas, 'tipos': tipos})
+
+def lanzar_estrategia(request):
+    estrategias = Estrategia.objects.all()
+    return render(request, 'pruebas_app/lanzar_estrategia.html', {'estrategias': estrategias} )
