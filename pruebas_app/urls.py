f7ceb3f7212f4296a606de945fcb92a932dde4a9
Nicolas Lema <n.lema@uniandes.edu.co>
2020-02-17 00:34:38 -0500

    Solo mostrar versiones disponibles de la aplicacion escogida

--- pruebas_app/urls.py
+++ pruebas_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     path('agregar_estrategia/', views.agregar_estrategia, name='agregar_estrategia'),
     path('lanzar_estrategia/', views.lanzar_estrategia, name='lanzar_estrategia'),
+    path('obtener_versiones_de_una_estrategia/', views.obtener_versiones_de_una_estrategia, name='obtener_versiones_de_una_estrategia'),
 ]
--- pruebas_app/views.py
+++ pruebas_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Aplicacion, Version, Herramienta, Tipo, Estrategia
+from django.http import HttpResponse
+import json
 
 # Create your views here.
 
@@ -8,10 +10,25 @@ def home(request):
 
 def agregar_estrategia(request):
     aplicaciones = Aplicacion.objects.all()
-    versiones = Version.objects.all() #Hay que sacar la versiones de la app seleccionada, aqui hay un tuto https://simpleisbetterthancomplex.com/tutorial/2018/01/29/how-to-implement-dependent-or-chained-dropdown-list-with-django.html
+    versiones = Version.objects.all() #Hay que sacar la versiones de la app seleccionada, ver notas
     herramientas = Herramienta.objects.all()
     tipos = Tipo.objects.all()
     return render(request, 'pruebas_app/agregar_estrategia.html', {'aplicaciones': aplicaciones, 'versiones': versiones, 'herramientas': herramientas, 'tipos': tipos})
+
+def obtener_versiones_de_una_estrategia(request):
+    aplicacion_id = int(request.GET['aplicacion_id'])
+    print ("ajax aplicacion_id ", aplicacion_id)
+
+    result_set = []
+    versiones = []
+    aplicacion = Aplicacion.objects.get(id=aplicacion_id)
+    print ("selected app name ", aplicacion)
+    versiones = Version.objects.filter(aplicacion=aplicacion)
+    for v in versiones:
+        print ("version name", v.numero)
+        result_set.append({'numero': v.numero})
+    return HttpResponse(json.dumps(result_set), content_type='application/json')
+
 
 def lanzar_estrategia(request):
     estrategias = Estrategia.objects.all()
