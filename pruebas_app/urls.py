ac7c20e07cb6c30728c320ec5c7c201833b23154
Nicolas Lema <n.lema@uniandes.edu.co>
2020-02-23 12:47:00 -0500

    Guardar estrategias con sus respectivas pruebas

--- pruebas_app/urls.py
+++ pruebas_app/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('lanzar_estrategia/', views.lanzar_estrategia, name='lanzar_estrategia'),
     path('obtener_versiones_de_una_estrategia/', views.obtener_versiones_de_una_estrategia, name='obtener_versiones_de_una_estrategia'),
     path('guardar_estrategia/', views.guardar_estrategia, name='guardar_estrategia'),
-    path('agregar_scripts/<int:estrategia_id>', views.agregar_scripts, name='agregar_scripts'),
-    path('guardar_script/<int:estrategia_id>', views.guardar_script, name='guardar_script'),
+    path('agregar_scripts/<int:estrategia_id>', views.agregar_prueba, name='agregar_scripts'),
+    path('guardar_prueba/<int:estrategia_id>', views.guardar_prueba, name='guardar_prueba'),
+    path('eliminar_prueba/<int:prueba_id>', views.eliminar_prueba, name='eliminar_prueba'),
+    path('eliminar_estrategia/<int:estrategia_id>', views.eliminar_estrategia, name='eliminar_estrategia'),
 ]
--- pruebas_app/views.py
+++ pruebas_app/views.py
@@ -23,22 +23,41 @@ def guardar_estrategia(request):
         descripcion_estrategia = request.POST['descripcion_estrategia']
         estrategia = Estrategia(nombre=nombre_estrategia, descripcion=descripcion_estrategia, version=version)
         estrategia.save()
-        return HttpResponseRedirect(reverse('agregar_scripts', args=(estrategia.id,)))
+        return HttpResponseRedirect(reverse('agregar_prueba', args=(estrategia.id,)))
 
-def agregar_scripts(request, estrategia_id):
+def eliminar_estrategia(request, estrategia_id):
+    estrategia = Estrategia.objects.get(id=estrategia_id)
+    estrategia.delete()
+    return lanzar_estrategia(request)
+
+def agregar_prueba(request, estrategia_id):
     estrategia = Estrategia.objects.get(id=estrategia_id)
     herramientas = Herramienta.objects.all()
     tipos = Tipo.objects.all()
     pruebas = Prueba.objects.filter(estrategia=estrategia)
-    return render(request, 'pruebas_app/agregar_scripts.html', {'aplicacion': estrategia.version.aplicacion, 'version': estrategia.version, 'estrategia': estrategia, 'herramientas': herramientas, 'tipos': tipos, 'pruebas': pruebas})
+    return render(request, 'pruebas_app/agregar_prueba.html', {'aplicacion': estrategia.version.aplicacion, 'version': estrategia.version, 'estrategia': estrategia, 'herramientas': herramientas, 'tipos': tipos, 'pruebas': pruebas})
 
-def guardar_script( request, estrategia_id):
+def guardar_prueba( request, estrategia_id):
     if request.method == 'POST':
-        
-        
+       
+        _, script = request.FILES.popitem() 
+        script = script[0]
+        herramienta = Herramienta.objects.get(id=request.POST['herramienta'])
+        tipo = Tipo.objects.get(id=request.POST['tipo'])
+        estrategia = Estrategia.objects.get(id=estrategia_id)
+        prueba = Prueba(script=script, herramienta=herramienta, tipo=tipo, estrategia=estrategia)
+        prueba.save()
+
         print(request.POST)
         print(request.FILES)
-        pass
+        return agregar_prueba(request, estrategia_id)
+
+def eliminar_prueba(request, prueba_id):
+    prueba = Prueba.objects.get(id=prueba_id)
+    estrategia_id = prueba.estrategia.id
+    prueba.delete()
+    return agregar_prueba(request, estrategia_id)
+
 
 def obtener_versiones_de_una_estrategia(request):
     aplicacion_id = int(request.GET['aplicacion_id'])
