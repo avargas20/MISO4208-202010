f5bb339e450320dd53053d3de9dc3f52746be1c4
Nicolas Lema <n.lema@uniandes.edu.co>
2020-02-23 21:48:39 -0500

    Implementacion cypress

--- scripts/worker_cypress.py
+++ scripts/worker_cypress.py
@@ -10,9 +10,21 @@ from pruebas_app.models import Aplicacion, Prueba, Version, Herramienta, Tipo, E
 
 def funcion(resultado_id):
 
-    print(resultado_id)
+    print("En el resultado:",resultado_id)
     resultado = Resultado.objects.get(id=resultado_id)
     prueba = resultado.prueba
-    print(prueba.script)
+    print("El script es:",prueba.script)
 
-    salida = subprocess.call(['npx', 'cypress', 'run'], shell=True, cwd=settings.CYPRESS_PATH)
+    f=open(prueba.script.path, "r")
+    contenido = f.read()
+    print("El contenido es:",contenido)
+    nuevo_archivo = 'cypress/integration/'+str(resultado.id)+".js"
+    ruta_nuevo_ejecutable = os.path.join(settings.CYPRESS_PATH, nuevo_archivo)
+    with open(ruta_nuevo_ejecutable, "w+") as file:
+        file.write(contenido)
+    
+    salida = subprocess.call(['npx', 'cypress', 'run', 'cypress:run', '--spec', nuevo_archivo], shell=True, cwd=settings.CYPRESS_PATH)
+
+    print('La salida es:',salida)
+    
+
