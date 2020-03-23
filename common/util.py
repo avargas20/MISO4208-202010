e1f46171e81e8d06ed39b26e83b276eecad9ee42
stillirrom <irrom@hotmail.com>
2020-03-22 21:45:00 -0500

    *Agregando Monkey base *Formateo y agregando nuevo método que incluye reemplazar *Nuevo worker creado *Agregando lógica para aleatorias-web

--- common/util.py
+++ common/util.py
@@ -7,6 +7,7 @@ from django.conf import settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pruebas_automaticas.settings")
 django.setup()
 
+
 # Este metodo se encarga de copiar el codigo del script de la prueba en la ruta donde la herramienta necesite ese script para poder correr la prueba
 def copiar_contenido(resultado, ruta_herramienta, ruta_interna, extension_archivo):
     print("En el resultado:", resultado)
@@ -16,7 +17,7 @@ def copiar_contenido(resultado, ruta_herramienta, ruta_interna, extension_archiv
     archivo = open(prueba.script.path, "r")
     contenido = archivo.read()
     print("El contenido es:", contenido)
-    nuevo_archivo = ruta_interna+str(resultado.id)+extension_archivo
+    nuevo_archivo = ruta_interna + str(resultado.id) + extension_archivo
     ruta_nuevo_ejecutable = os.path.join(ruta_herramienta, nuevo_archivo)
     with open(ruta_nuevo_ejecutable, "w+") as file:
         file.write(contenido)
@@ -25,7 +26,7 @@ def copiar_contenido(resultado, ruta_herramienta, ruta_interna, extension_archiv
 
 # Este metodo valida que si la solicitud esta terminada, debe agrupar todos los resultados en un solo .zip y guardarlos como evidencias, todos los workers lo deben llamar
 def validar_ultimo(solicitud):
-    if(solicitud.terminada):
+    if (solicitud.terminada):
         zip_objetcs = ZipFile('evidencias.zip', 'w')
         for r in solicitud.resultado_set.all():
             if bool(r.resultado):
@@ -33,8 +34,28 @@ def validar_ultimo(solicitud):
             if bool(r.log):
                 zip_objetcs.write(r.log.path, r.log.name)
         zip_objetcs.close()
-        archivo = open(settings.BASE_DIR+"//evidencias.zip", 'rb')    
+        archivo = open(settings.BASE_DIR + "//evidencias.zip", 'rb')
         archivo_zip = File(archivo)
         solicitud.evidencia.save('evidencias.zip', archivo_zip, save=True)
         archivo_zip.close()
-        os.remove(settings.BASE_DIR+"//evidencias.zip")
+        os.remove(settings.BASE_DIR + "//evidencias.zip")
+
+
+# Este metodo se encarga de copiar el codigo del script de la prueba en la ruta donde la herramienta necesite ese script para poder correr la prueba
+# , reemplazando el token por la URL del archivo
+def copiar_contenido_token(resultado, ruta_herramienta, ruta_interna, extension_archivo):
+    print("En el resultado:", resultado)
+    prueba = resultado.prueba
+    prueba.script = "Monkey.js"
+    print("El script es:", prueba.script)
+    url = resultado.solicitud.estrategia.version.url
+
+    archivo = open(prueba.script.path, "r")
+    contenido = archivo.read()
+    contenido = contenido.replace('-urlToken-', url)
+    print("El contenido luego de reemplazar es:", contenido)
+    nuevo_archivo = ruta_interna + str(resultado.id) + extension_archivo
+    ruta_nuevo_ejecutable = os.path.join(ruta_herramienta, nuevo_archivo)
+    with open(ruta_nuevo_ejecutable, "w+") as file:
+        file.write(contenido)
+    return nuevo_archivo
