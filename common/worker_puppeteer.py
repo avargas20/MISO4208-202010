62972aad8dc085ff3b05175361ff5a5170aa60bb
Daury <daurylecca16@gmail.com>
2020-03-29 19:32:57 -0500

    Se ajusta worker puppeteer

--- common/worker_puppeteer.py
+++ common/worker_puppeteer.py
@@ -6,9 +6,9 @@ from django.core.files import File
 from common import util
 from pruebas_automaticas import settings
 from pruebas_app.models import Resultado
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pruebas_app.settings")
 django.setup()
-
 
 SQS = boto3.resource('sqs', region_name='us-east-1')
 COLA_PUPPETEER = SQS.get_queue_by_name(QueueName=settings.SQS_PUPPETEER_NAME)
@@ -18,13 +18,14 @@ if __name__ == '__main__':
     while True:
         for message in COLA_PUPPETEER.receive_messages(MaxNumberOfMessages=1, MessageAttributeNames=['Id']):
             if message.message_attributes is not None:
-
                 resultado_id = message.message_attributes.get('Id').get('StringValue')
                 resultado = Resultado.objects.get(id=int(resultado_id))
-    
-                nuevo_archivo = util.copiar_contenido(resultado, settings.PUPPETEER_PATH, settings.RUTAS_INTERNAS["Puppeteer"], '.js')
 
-                salida = subprocess.call(['jest', nuevo_archivo], shell=True, cwd=settings.PUPPETEER_PATH)
+                nuevo_archivo = util.copiar_contenido(resultado, settings.PUPPETEER_PATH,
+                                                      settings.RUTAS_INTERNAS["Puppeteer"], '.test.js')
+
+                salida = subprocess.run(['jest', nuevo_archivo], shell=True, check=False, cwd=settings.PUPPETEER_PATH,
+                                        stdout=subprocess.PIPE)
 
                 print('Salida:', salida.stdout.decode('utf-8'))
 
