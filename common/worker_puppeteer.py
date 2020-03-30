5352064b4f59c89744ea4c86d0e035684ce9f8bf
Adriana Vargas <a.vargas20@uniandes.edu.co>
2020-03-30 14:28:12 -0500

    Worker puppeteer totalmente funcional

--- common/worker_puppeteer.py
+++ common/worker_puppeteer.py
@@ -6,9 +6,9 @@ from django.core.files import File
 from common import util
 from pruebas_automaticas import settings
 from pruebas_app.models import Resultado
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pruebas_app.settings")
 django.setup()
-
 
 SQS = boto3.resource('sqs', region_name='us-east-1')
 COLA_PUPPETEER = SQS.get_queue_by_name(QueueName=settings.SQS_PUPPETEER_NAME)
@@ -18,23 +18,29 @@ if __name__ == '__main__':
     while True:
         for message in COLA_PUPPETEER.receive_messages(MaxNumberOfMessages=1, MessageAttributeNames=['Id']):
             if message.message_attributes is not None:
-
                 resultado_id = message.message_attributes.get('Id').get('StringValue')
                 resultado = Resultado.objects.get(id=int(resultado_id))
-    
-                nuevo_archivo = util.copiar_contenido(resultado, settings.PUPPETEER_PATH, settings.RUTAS_INTERNAS["Puppeteer"], '.js')
 
-                salida = subprocess.call(['jest', nuevo_archivo], shell=True, cwd=settings.PUPPETEER_PATH)
+                nuevo_archivo = util.copiar_contenido(resultado, settings.PUPPETEER_PATH, settings.RUTAS_INTERNAS['Puppeteer'], '.test.js')
 
-                print('Salida:', salida.stdout.decode('utf-8'))
+                comando = subprocess.run(['jest', nuevo_archivo], shell=True, stdout=subprocess.PIPE, cwd=settings.PUPPETEER_PATH)
+                print('Salida:', comando.stdout.decode('utf-8'))
 
-                archivo_log = open(str(resultado.id) + "log.txt", "w+")
-                archivo_log.write(salida.stdout.decode('utf-8'))
-
-                resultado.log.save('log_puppeteer.txt', archivo_log, save=True)
+                reporte = open(settings.PUPPETEER_PATH + 'test-report.html', 'rb')
+                archivo_reporte = File(reporte)
+                resultado.resultado.save('reporte_puppeteer.html', archivo_reporte, save=True)
                 resultado.terminada = True
                 resultado.save()
-                archivo_log.close()
-                os.remove(archivo_log.name)
+                reporte.close()
+                os.remove(reporte.name)
+
+                #Se borra script anterior
+                script = open(settings.PUPPETEER_PATH + nuevo_archivo, 'rb')
+                os.remove(script.name)
+
                 message.delete()
                 util.validar_ultimo(resultado.solicitud)
+
+                print("Final del worker")
+
+                exit()
