fe855a875b0b7925e96cf701fa3e70bc05cf4d55
Adriana Vargas <a.vargas20@uniandes.edu.co>
2020-03-31 18:21:48 -0500

    Se agregan clases al admin django

--- pruebas_app/admin.py
+++ pruebas_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Aplicacion, Version, Estrategia, Herramienta, Tipo, Prueba, Estado, Solicitud, TipoAplicacion, Resultado
+from .models import Aplicacion, Version, Estrategia, Herramienta, Tipo, Prueba, Estado, Solicitud, TipoAplicacion, \
+    Resultado, ResultadoVRT, ScreenShot
 
 # Register your models here.
 admin.site.register(Aplicacion)
@@ -13,3 +14,5 @@ admin.site.register(Estado)
 admin.site.register(Solicitud)
 admin.site.register(TipoAplicacion)
 admin.site.register(Resultado)
+admin.site.register(ResultadoVRT)
+admin.site.register(ScreenShot)
