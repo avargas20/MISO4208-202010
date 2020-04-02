8cecfd5afd63144abccb6e95e757b3f6e41cdc40
Adriana Vargas <a.vargas20@uniandes.edu.co>
2020-04-02 17:18:41 -0500

    Se incluye validación de resultados vrt

--- common/util.py
+++ common/util.py
@@ -1,4 +1,6 @@
+import json
 import os
+from decimal import Decimal
 from zipfile import ZipFile
 from django.core.files import File
 import django
@@ -54,38 +56,59 @@ def validar_ultimo(solicitud):
 
 # Este metodo se ejecuta cuando una solicitud tiene VRT y lo que hace es recorrer todos los resultados de ambas solicitudes y todos sus screenshoots y a cada screensoot reciproco
 # le ejecuta VRT
+def validar_resultado_vrt(informacion, sensibilidad_VRT, resultado_vrt):
+    if informacion > sensibilidad_VRT:
+        resultado_vrt.fallida = True
+    else:
+        resultado_vrt.fallida = False
+
+    resultado_vrt.save()
+    return r
+
 def ejecutar_vrt(solicitud_posterior):
     # Se sacan los resultados de ambas solicitudes
     resultados_posteriores = solicitud_posterior.resultado_set.all()
-    resultados_anteriores = solicitud_posterior.solicitud_VRT.resultado_set.all()
     print('posteriores:', resultados_posteriores)
+    resultados_anteriores = solicitud_posterior.solicitud_VRT.resultado_set.all()
     print('anteriores:', resultados_anteriores)
+
     # Se recorren los resultados, note que para que funcione deben tener la misma cantidad de resultados y haberse ejecutado en el mismo orden
     for i in range(resultados_anteriores.count()):
         # de cada resultado de cada solicitud se sacan los screenshots
         screenshots_posteriores = resultados_posteriores[i].screenshot_set.all()
-        screenshots_anteriores = resultados_anteriores[i].screenshot_set.all()
         print('screen posteriores:', screenshots_posteriores)
+        screenshots_anteriores = resultados_anteriores[i].screenshot_set.all()
         print('screen anteriores:', screenshots_anteriores)
+
         # Se recorre cada screenshot de cada resultado y esos son los que se comparan
         for j in range(screenshots_anteriores.count()):
             resultado_vrt = ResultadoVRT()
             resultado_vrt.solicitud = solicitud_posterior
             resultado_vrt.save()
+
             # las imagenes para comparar no es necesario subirlas nuevamente, solo se referencian las originales
             imagen_posterior = screenshots_posteriores[j].imagen
             imagen_anterior = screenshots_anteriores[j].imagen
+
             # para la imagen diferencias se crea una ruta ficticia en la cual resemble creara la nueva imagen
             imagen_diferencias = settings.BASE_DIR + '//archivos//screenshots//VRT//' + str(resultado_vrt.id) + '.png'
             resultado_vrt.screenshoot_posterior = imagen_posterior
             resultado_vrt.screenshoot_previo = imagen_anterior
             resultado_vrt.save()
+
             salida = subprocess.run(
                 ['node', 'index.js', resultado_vrt.screenshoot_previo.path, resultado_vrt.screenshoot_posterior.path,
                  imagen_diferencias], shell=True, cwd=settings.RESEMBLE_PATH, stdout=subprocess.PIPE)
-            print("la salida es:", salida.stdout)
+
             resultado_vrt.informacion = salida.stdout.decode('utf-8')
+            print("la salida es:", resultado_vrt.informacion)
+
+            json_content = json.dumps(resultado_vrt.informacion)
+            misMatchPercentage = json_content.split("'")[1].split("'")[-1]
+            validar_resultado_vrt(Decimal(misMatchPercentage), solicitud_posterior.sensibilidad_VRT, resultado_vrt)
+
             # una vez resemble creo la imagen esta se guarda nuevamente pero en la solicitud_vrt y luego se borra la original
+            print(imagen_diferencias)
             imagen_diff = open(imagen_diferencias, 'rb')
             resultado_vrt.imagen_diferencias.save('diferencia.png', File(imagen_diff), save=True)
             imagen_diff.close()
@@ -94,16 +117,14 @@ def ejecutar_vrt(solicitud_posterior):
 
 
 # Este metodo busca recoger todos los screenshoots tomados por los scripts y guardarlos en la tabla ScreenShoot
-def recoger_screenshoots(resultado):
-    # La ruta depende de la herramienta que ejecuto la prueba
-    ruta_a_recoger = determinar_ruta(resultado)
+def recoger_screenshoots(resultado, ruta_a_recoger):
     # Se crea una expresion con *.png para que recoja esos tipos de archivos
     ruta_con_filtro = os.path.join(ruta_a_recoger, '*.png')
     imagenes = glob.glob(ruta_con_filtro)
+
     for imagen in imagenes:
         print('screens:', imagen)
-        # Se saca el nombre del archivo sin extension
-        nombre = imagen.split('\\')[-1].split('.')[0]
+        nombre = imagen.split("\\")[-1]
         screenshoot = ScreenShot()
         screenshoot.resultado = resultado
         # Se guarda para generar el id y que quede guardado el screenshoot con ese id
