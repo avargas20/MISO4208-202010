98d20d62d0f1eae3ee8fe04e24f2646a1cdfd7b1
Adriana Vargas <a.vargas20@uniandes.edu.co>
2020-05-01 14:56:42 -0500

    Se agregan validaciones a herramientas por tipos de aplicacion

--- pruebas_app/models.py
+++ pruebas_app/models.py
@@ -8,6 +8,7 @@ from django.db import models
 
 class TipoAplicacion(models.Model):
     tipo = models.CharField(max_length=30)
+    herramientas = models.ManyToManyField('Herramienta')
 
     def __str__(self):
         return '%s' % self.tipo
